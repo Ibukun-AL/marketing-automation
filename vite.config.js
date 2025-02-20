f3cb64e5f8617a2b45e1844ab7b38cafd0d9f774
IBUKUN-BIT <alukoibk@icloud.com>
2025-02-20 12:43:06 +0100

    update
    update

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 3000,
+    allowedHosts: process.env.VITE_ALLOWED_HOST ? [process.env.VITE_ALLOWED_HOST] : [],
   },
 })
